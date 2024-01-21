306a445b6ee042e934021939133cf1ecf824b712
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 09:48:16 -0500

    bread controller

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
+// BREADS
+const breadsController = require(`.controllers/breads_controller.js`)
+app.use(`/breads`, breadsController)
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
