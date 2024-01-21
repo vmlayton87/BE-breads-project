ed483feac982b0ed895aa779d2ff97081274d448
vmlayton87 <vmlayton87@gmail.com>
2024-01-21 11:01:36 -0500

    add arrayIndex route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,4 +7,9 @@ breads.get(`/`, (req, res)=>{
     res.send(Bread)
 })
 
+// ROUTE
+breads.get(`/:arrayIndex`, (req, res)=>{
+    res.send(Bread[req.params.arrayIndex])
+})
+
 module.exports = breads
