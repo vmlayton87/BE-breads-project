0ca4e79bdac5126a2219ecac2a09ad0dd063de95
vmlayton87 <vmlayton87@gmail.com>
2024-01-23 20:25:29 -0500

    new form and buttons

--- server.js
+++ server.js
@@ -11,16 +11,17 @@ app.set('views', __dirname + '/views') // explicit dependency
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static(`public`))
+app.use(express.urlencoded({extended: true}))
+
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+  res.render('layouts/default')
 })
 
 // BREADS
 const breadsController = require(`./controllers/breads_controller.js`)
 app.use(`/breads`, breadsController)
-
 
 // 404 Page
 app.get('*', (req, res) => {
--- views/index.jsx
+++ views/index.jsx
@@ -22,6 +22,10 @@ function Index ({breads, title}) {
                 })
             }
         </ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
       </Default>
     )
 }
