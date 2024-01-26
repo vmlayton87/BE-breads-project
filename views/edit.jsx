88602eb36cadb1a86031599428b97f92f2dd6831
vmlayton87 <vmlayton87@gmail.com>
2024-01-25 21:16:12 -0500

    edit view rendering

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
-const Default = require('../layouts/Default')
+const Default = require('./layouts/default')
 
-function Edit () {
+function Edit ({bread, index}) {
     return (
       <Default>
         <h2>Edit a bread</h2>
@@ -12,18 +12,21 @@ function Edit () {
             name="name"
             id="name"
             required
+            defaultValue={bread.name}
           />
           <label htmlFor="image">Image</label>
           <input
             type="text"
             name="image"
-            id="image"/>
+            id="image"
+            defaultValue={bread.image} 
+            />
           <label htmlFor="hasGluten">Has Gluten?</label>
           <input
             type="checkbox"
             name="hasGluten"
             id="hasGluten"
-            defaultChecked
+            defaultChecked={bread.hasGluten}
           />
           <br />
           <input type="submit"/>
