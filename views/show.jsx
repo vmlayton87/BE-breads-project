30339302a096ac779b41f7003111ccf58f865627
vmlayton87 <vmlayton87@gmail.com>
2024-01-25 21:19:43 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -18,6 +18,9 @@ function Show ({bread, index}) {
         </p>
         <img src={bread.image} alt={bread.name} />
         <li><a href="/breads">Go Home</a></li>
+        
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type="submit" value="DELETE"/>
         </form>
